a977e44eb8728a846f752802fad4a2e14444ad6b
pianowithalex <59804251+pianowithalex@users.noreply.github.com>
2021-04-06 14:26:59 +1200

    lots of work

---
+++ simple_platformer/adding_levels.py
@@ -0,0 +1,138 @@
+import arcade
+WIDTH = 1000
+HEIGHT = 650
+
+TITLE = "TMX Playground"
+
+#scaling
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
+SPRITE_PIXEL_SIZE = 128
+GRID_PIXEL_SIZE = (SPRITE_PIXEL_SIZE * TILE_SCALING)
+
+PLAYER_MOVEMENT_SPEED = 10
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
+
+VIEWPORT_MARGIN = 100
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(WIDTH, HEIGHT, TITLE)
+        self.coin_list = None
+        self.wall_list = None
+        self.player_list = None
+        self.player_sprite = None
+        self.physics_engine = None
+        self.view_bottom = 0
+        self.view_left = 0
+        self.score = 0
+
+        arcade.set_background_color(arcade.color.CORNFLOWER_BLUE)
+
+    def setup(self):
+        self.view_bottom = 0
+        self.view_left = 0
+        self.score = 0
+        self.coin_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+        self.player_list = arcade.SpriteList()
+
+        self.player_sprite = arcade.Sprite("./PNG/Adventurer/Poses/adventurer_cheer1.png", CHARACTER_SCALING)
+        self.player_sprite.center_x = 300
+        self.player_sprite.center_y = 300
+        self.player_list.append(self.player_sprite)
+
+        self.load_map('./Maps/platformer.tmx')
+
+    def load_map(self, resource):
+        platforms_layer_name = 'Platforms'
+        coins_layer_name = 'Coins'
+
+        my_map = arcade.tilemap.read_tmx(resource)
+
+        self.wall_list = arcade.tilemap.process_layer(
+            map_object=my_map, 
+            layer_name=platforms_layer_name, 
+            scaling=TILE_SCALING, 
+            use_spatial_hash=True)
+
+        self.coin_list = arcade.tilemap.process_layer(my_map, coins_layer_name, TILE_SCALING)
+
+        if my_map.background_color:
+            arcade.set_background_color(my_map.background_color)
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
+    def scroll(self):
+        changed = False
+
+        # Scroll left
+        left_boundary = self.view_left + VIEWPORT_MARGIN
+        if self.player_sprite.left < left_boundary:
+            self.view_left -= left_boundary - self.player_sprite.left
+            changed = True
+
+        right_boundary = self.view_left + WIDTH - VIEWPORT_MARGIN
+        if self.player_sprite.right > right_boundary:
+            self.view_left +=  self.player_sprite.right - right_boundary
+            changed = True
+
+        top_boundary = self.view_bottom + HEIGHT - VIEWPORT_MARGIN
+        if self.player_sprite.top > top_boundary:
+            self.view_bottom += self.player_sprite.top - top_boundary
+            changed = True
+
+        bottom_boundary = self.view_bottom + VIEWPORT_MARGIN
+        if self.player_sprite.bottom < bottom_boundary:
+            self.view_bottom -= bottom_boundary - self.player_sprite.bottom
+            changed = True
+
+        self.view_left = int(self.view_left)
+        self.view_bottom = int(self.view_bottom)
+
+        if changed:
+            arcade.set_viewport(self.view_left, 
+            WIDTH + self.view_left - 1, 
+            self.view_bottom, 
+            HEIGHT + self.view_bottom - 1)
+    
+    def on_draw(self):
+        arcade.start_render()
+        self.wall_list.draw()
+        self.coin_list.draw()
+        self.player_list.draw()
+
+        score_text = f"Score: {self.score}"
+        arcade.draw_text(score_text, 10 + self.view_left, 10 + self.view_bottom, arcade.csscolor.WHITE, 18)
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+            
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+            
+    def on_update(self, delta_time):
+        self.physics_engine.update()
+
+        coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+
+        for coin in coin_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+        
+        self.scroll()
+
+window = MyGame()
+window.setup()
+arcade.run()
